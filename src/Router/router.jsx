7780ec8f5f3ee69e9e3b62a54961ef1eb5cb0c58
mahmudul11223344 <islamashraf128@gmail.com>
2025-06-26 20:24:00 +0600

    first commit

---
+++ src/Router/router.jsx
@@ -0,0 +1,30 @@
+import {
+  createBrowserRouter,
+} from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home/Home/Home";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Authentication/Login/Login";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+        {
+            index: true,
+            Component: Home
+        }
+    ]
+  },
+  {
+    path: '/',
+    Component: AuthLayout,
+    children: [
+      {
+        path: 'login',
+        Component:Login
+      }
+    ]
+  }
+]);
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  RouterProvider,
+} from "react-router";
+import { router } from './Router/router';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
+AOS.init();
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <div className='font-urbanist max-w-7xl mx-auto'>
+      <RouterProvider router={router} />
+    </div>
+  </StrictMode>,
+)
