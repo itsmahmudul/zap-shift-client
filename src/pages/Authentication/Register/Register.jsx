93a3c58359a6d7f16abfb8d6c629cdb5d3b45859
mahmudul11223344 <islamashraf128@gmail.com>
2025-07-23 14:36:22 +0600

    long time

--- src/pages/Authentication/Register/Register.jsx
+++ src/pages/Authentication/Register/Register.jsx
@@ -1,22 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import useAuth from '../../../hooks/useAuth';
 import { Link } from 'react-router';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import axios from 'axios';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const Register = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const { createUser } = useAuth();
+    const { createUser, updateUserProfile } = useAuth();
+    const [profilePic, setProfilePic] = useState('');
+    const axiosInstance = useAxiosSecure();
 
     const onSubmit = data => {
+
         console.log(data);
+
         createUser(data.email, data.password)
-            .then(result => {
-                console.log(result.user)
+            .then(async (result) => {
+                console.log(result.user);
+
+                // update userinfo in the database
+                const userInfo = {
+                    email: data.email,
+                    role: 'user', // default role
+                    created_at: new Date().toISOString(),
+                    last_log_in: new Date().toISOString()
+                }
+
+                const userRes = await axiosInstance.post('/users', userInfo);
+                console.log(userRes.data);
+
+                // update user profile in firebase
+                const userProfile = {
+                    displayName: data.name,
+                    photoURL: profilePic
+                }
+                updateUserProfile(userProfile)
+                    .then(() => {
+                        console.log('profile name pic updated')
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+
             })
             .catch(error => {
                 console.error(error);
             })
+    }
+
+    const handleImageUpload = async (e) => {
+        const image = e.target.files[0];
+        console.log(image)
+
+        const formData = new FormData();
+        formData.append('image', image);
+
+
+        const imagUploadUrl = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_image_upload_key}`
+        const res = await axios.post(imagUploadUrl, formData)
+
+        setProfilePic(res.data.data.url);
+
     }
 
     return (
@@ -26,6 +72,20 @@ const Register = () => {
                 <h1 className="text-5xl font-bold">Create Account</h1>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <fieldset className="fieldset">
+                        {/* name field */}
+                        <label className="label">Your Name</label>
+                        <input type="text"
+                            {...register('name', { required: true })}
+                            className="input" placeholder="Your Name" />
+                        {
+                            errors.email?.type === 'required' && <p className='text-red-500'>Name is required</p>
+                        }
+                        {/* name field */}
+                        <label className="label">Your Name</label>
+                        <input type="file"
+                            onChange={handleImageUpload}
+                            className="input" placeholder="Your Profile picture" />
+
                         {/* email field */}
                         <label className="label">Email</label>
                         <input type="email"
@@ -34,7 +94,7 @@ const Register = () => {
                         {
                             errors.email?.type === 'required' && <p className='text-red-500'>Email is required</p>
                         }
-
+                        {/* password field*/}
                         <label className="label">Password</label>
                         <input type="password" {...register('password', { required: true, minLength: 6 })} className="input" placeholder="Password" />
                         {
